707d94d801819a9ee7e43c0e41fc7dade42beef9
Walter Restifo <walterrestifo93@gmail.com>
2022-11-22 07:48:29 +0100

    fix the issues noted by Antje

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header({ title }) {
   return (
     <header className="header">
-      <h1 className="h1">{title}</h1>
+      <h1 className="header__h1">{title}</h1>
     </header>
   );
 }
